3cb4881c1d73b302c2c61cadfc2020bad5c367c2
haazime <h4zime@gmail.com>
2013-10-20 14:33:18 +0900

    complete java_version.rb

--- lib/java_version.rb
+++ lib/java_version.rb
@@ -1,9 +1,10 @@
 class JavaVersion
+  include Comparable
 
   class << self
 
     def valid?(version)
-      return false unless /^JDK\d+u[1-9]\d+$/ =~ version
+      return false unless /^JDK\d+u\d+$/ =~ version
       return true
     end
 
@@ -18,5 +19,37 @@ class JavaVersion
   def initialize(family_number, update_number)
     @family_number = family_number
     @update_number = update_number
+  end
+
+  def next_limited_update
+    self.class.new(
+      @family_number,
+      20 * (@update_number / 20 + 1)
+    )
+  end
+
+  def next_critical_patch_update
+    self.class.new(
+      @family_number,
+      (5 * (@update_number / 5 + 1)).tap do |x|
+        break x + 1 if x % 2 == 0
+      end
+    )
+  end
+
+  def next_security_alert
+    self.class.new(
+      @family_number,
+      (@update_number + 1).tap do |x|
+        break x + 2 if x % 20 == 0 and x % 5 == 0
+        break x + 1 if x % 5 == 0
+      end
+    )
+  end
+
+  def <=>(other)
+    (self.family_number <=> other.family_number).tap do |x|
+      break self.update_number <=> other.update_number if x == 0
+    end
   end
 end
