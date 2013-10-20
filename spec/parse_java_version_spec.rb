fb117824231b06652dc852b9ef7d092362bbe0b1
haazime <h4zime@gmail.com>
2013-10-20 12:11:15 +0900

    add spec for parse

---
+++ spec/parse_java_version_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Parser Java Version" do
+  subject do
+    JavaVersion.parse(version)
+  end
+
+  describe "given JDK7u40" do
+    let(:version) { "JDK7u40" }
+    it { expect(subject.family_number).to eq(7) }
+    it { expect(subject.update_number).to eq(40) }
+  end
+
+  describe "given JDK6u51" do
+    let(:version) { "JDK6u51" }
+    it { expect(subject.family_number).to eq(6) }
+    it { expect(subject.update_number).to eq(51) }
+  end
+
+  describe "given JDKVersion" do
+    let(:version) { "JDKVersion" }
+    it { expect { subject }.to raise_error(ArgumentError) }
+  end
+
+  describe "given JDK8uABC" do
+    let(:version) { "JDK8uABC" }
+    it { expect { subject }.to raise_error(ArgumentError) }
+  end
+
+  describe "given JDKVu80" do
+    let(:version) { "JDKVu80" }
+    it { expect { subject }.to raise_error(ArgumentError) }
+  end
+end
