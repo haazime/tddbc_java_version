06da4b8587d4fde58c701516911a4d1d074ce5fc
haazime <h4zime@gmail.com>
2013-10-20 11:43:58 +0900

    rename spec to function

---
+++ spec/validate_java_version_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "Validate Java Version" do
+  subject do
+    JavaVersion.valid?(version)
+  end
+
+  describe "given JDK7u40" do
+    let(:version) { "JDK7u40" }
+    it { expect(subject).to be_true }
+  end
+
+  describe "given foo" do
+    let(:version) { "foo" }
+    it { expect(subject).to be_false }
+  end
+
+  describe "given JDK7u9x" do
+    let(:version) { "JDK7u9x" }
+    it { expect(subject).to be_false }
+  end
+
+  describe "given JDK123u0" do
+    let(:version) { "JDK123u0" }
+    it { expect(subject).to be_false }
+  end
+end
