4a7eb1af2881cf942f7d5241654e46cab13eb643
haazime <h4zime@gmail.com>
2013-10-20 12:10:41 +0900

    parse java version

--- lib/java_version.rb
+++ lib/java_version.rb
@@ -6,5 +6,17 @@ class JavaVersion
       return false unless /^JDK\d+u[1-9]\d+$/ =~ version
       return true
     end
+
+    def parse(version)
+      raise ArgumentError.new("invalid version `#{version}`") unless valid?(version)
+      new(*version.scan(/\d+/).collect(&:to_i))
+    end
+  end
+
+  attr_reader :family_number, :update_number
+
+  def initialize(family_number, update_number)
+    @family_number = family_number
+    @update_number = update_number
   end
 end
