1437022395b4c0f1fc02f474a6b92cd06dca22e2
haazime <h4zime@gmail.com>
2013-10-22 13:12:40 +0900

    fix next security alert

--- spec/next_java_version_spec.rb
+++ spec/next_java_version_spec.rb
@@ -21,12 +21,24 @@ describe "Next Java Version" do
     let(:current) { JavaVersion.parse("JDK7u44") }
 
     it do
+      expect(current.next_limited_update.update_number).to eq(60)
+    end
+
+    it do
+      expect(current.next_critical_patch_update.update_number).to eq(45)
+    end
+
+    it do
       expect(current.next_security_alert.update_number).to eq(46)
     end
   end
 
   describe "when current is JDK7u59" do
     let(:current) { JavaVersion.parse("JDK7u59") }
+
+    it do
+      expect(current.next_critical_patch_update.update_number).to eq(61)
+    end
 
     it do
       expect(current.next_security_alert.update_number).to eq(62)
@@ -37,7 +49,11 @@ describe "Next Java Version" do
     let(:current) { JavaVersion.parse("JDK7u69") }
 
     it do
-      expect(current.next_security_alert.update_number).to eq(71)
+      expect(current.next_critical_patch_update.update_number).to eq(71)
+    end
+
+    it do
+      expect(current.next_security_alert.update_number).to eq(72)
     end
   end
 end
